1526e6710e5aa92d3fc510f27adb4d9873e928ba
Auryn Macmillan <aurynmacmillan@gmail.com>
2020-06-11 22:00:20 -0400

    Replace if and revert with require

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -6,19 +6,19 @@ contract Registry {
 
   // Add any address provided as a parameter to the registry.
   function addRegistrant(address _registrant) public {
-    if(registrantIndex[_registrant] != 0){
-      revert();
-    }
-    require(registrants.length < 3126,"list is full");
+    require(registrantIndex[_registrant] != 0,"address already in registry")
+    require(registrants.length <= 3125,"registry is full");
     registrants.push(_registrant);
     registrantIndex[_registrant] = registrants.length - 1;
     emit newRegistrant(_registrant, registrantIndex[_registrant], registrants[registrants.length - 1]);
   }
 
+  // Get the index of an address in the registry
   function getIndexByAddress(address _registrant) public returns (uint) {
     return registrantIndex[_registrant];
   }
 
+  // Get the address stored at an index in the registry
   function getAddressByIndex(uint _index) public returns (address) {
     return registrants[_index];
   }
